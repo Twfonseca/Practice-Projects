d280e6e35c1cc71b4fb2818c673fbb9dfc609b8f
Tawan Fonseca <82100530+Twfonseca@users.noreply.github.com>
2022-01-03 22:11:22 +0044

    started script
    Started script editions

---
+++ time-tracking-dashboard/script/script.js
@@ -0,0 +1,25 @@
+
+let daily= document.querySelector("#dailyT")
+let weekly= document.querySelector("#weeklyT")
+let monthly= document.querySelector("#monthlyT")
+let timeWork= document.querySelector("#hourW")
+let lTimeWork= document.querySelector("#lHourW")
+let timePlay= document.querySelector("#hourP")
+let lTimePlay= document.querySelector("#lHourP")
+let timeStudy= document.querySelector("#hourS")
+let lTimeStudy= document.querySelector("#lHourS")
+let timeExerc= document.querySelector("#hourE")
+let lTimeExerc= document.querySelector("#lHourE")
+let timeSocial= document.querySelector("#hourSo")
+let lTimeSocial= document.querySelector("#lHourSo")
+let timeSelfC= document.querySelector("#hourSc")
+let lTimeSelfC= document.querySelector("#lHourSc")
+
+
+
+
+daily.addEventListener("click", dailyTime())
+weekly.addEventListener("click", weeklyTime())
+monthly.addEventListener("click", monthlyTime())
+
+
